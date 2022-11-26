4422aba004b6e9c5984722c3b7ccc43a564ac827
Robert Bagdahn <robertbaggi@gmail.com>
2022-11-26 16:12:12 +0100

    added dummy temperature

--- sensor/urls.py
+++ sensor/urls.py
@@ -1,10 +1,12 @@
+from django.urls import include, path
+from rest_framework_nested import routers
 
-from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
-from sensor import views
+from . import views
+
+router = routers.SimpleRouter()
+router.register(r'temperature', views.TemperatureView)
+router.register(r'save-temperature', views.SaveTemperature, basename="temperature")
 
 urlpatterns = [
-    path('temperature/', views.TemperatureView.as_view()),
+    path('', include(router.urls)),
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
--- sensor/views.py
+++ sensor/views.py
@@ -1,7 +1,26 @@
 from rest_framework import generics
 from .models import Temperature
 from .serializers import TemperatureSerializer
+from rest_framework import mixins, viewsets, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.generics import GenericAPIView
 
-class TemperatureView(generics.ListCreateAPIView):
+from service.sensor import SensorModule
+
+class TemperatureView(viewsets.ModelViewSet):
     queryset = Temperature.objects.all()
     serializer_class = TemperatureSerializer
+
+
+class SaveTemperature(viewsets.ModelViewSet):
+
+    queryset = Temperature.objects.all()
+    serializer_class = TemperatureSerializer
+
+    def create(self, request, *args, **kwargs):
+        
+        SensorClass = SensorModule()
+        SensorClass.add_temperature()
+        return Response('Daten wurden geschrieben', status=status.HTTP_200_OK)
